a07e8d8e0bd99853c1854aa23b0a427ecf61e267
Zaxe <jmjacolbiapogi17@gmail.com>
2025-12-20 09:07:15 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1 class="text-red-500">hiiiii</h1>
+</body>
+</html>
