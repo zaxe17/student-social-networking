84af4dc705c4b4625e5d951f811d8f0015600f41
rupertmatthewaro <rupertmatthewaro@gmail.com>
2026-01-13 20:12:11 +0800

    controller and model

--- resources/views/page/signupin.blade.php
+++ resources/views/page/signupin.blade.php
@@ -10,78 +10,93 @@
         </div>
     </nav>
 
-    <!-- BODY -->
     <div class="container flex justify-center items-center mx-auto h-full p-10">
-        <!-- REGITRATION FORM -->
-        <form action="" method="" class="bg-[#f4f6f9]/60 rounded-3xl w-md h-full p-8 gap-1">
+        <form action="{{ route('students.store') }}" method="POST" class="bg-[#f4f6f9]/60 rounded-3xl w-md h-full p-8 gap-1">
+            @csrf
             <div class="flex justify-center items-center flex-col gap-1.5">
                 <h1 class="text-center font-medium text-3xl">Registration</h1>
                 <div class="grid grid-cols-12 gap-4">
-                    <!-- Student ID (col-span-12) -->
+
+                    <!-- Student ID -->
                     <div class="flex flex-col col-span-12">
-                        <label for="">Student ID</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="student_id">Student ID</label>
+                        <input type="text" name="student_id" id="student_id" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
                     </div>
 
-                    <!-- First Name (col-span-6) -->
+                    <!-- First Name -->
                     <div class="flex flex-col col-span-6">
-                        <label for="">First Name</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="first_name">First Name</label>
+                        <input type="text" name="first_name" id="first_name" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
                     </div>
 
-                    <!-- Last Name (col-span-6) -->
+                    <!-- Last Name -->
                     <div class="flex flex-col col-span-6">
-                        <label for="">Last Name</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" name="last_name" id="last_name" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
                     </div>
 
-                    <!-- Birthday (col-span-4) -->
-                    <div class="flex flex-col col-span-4">
-                        <label for="">Birthday</label>
-                        <input type="text" name="" placeholder="yyyy" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                    <!-- Birthday -->
+                    <div class="flex flex-col col-span-12">
+                        <label for="birthday">Birthday</label>
+                        <div class="flex gap-2">
+                            <!-- Year -->
+                            <select name="birth_year" class="text-sm bg-[#dde0e5] w-1/3 h-8 rounded-sm focus:outline-none" required>
+                                <option value="">Year</option>
+                                @for($year = date('Y'); $year >= 1900; $year--)
+                                    <option value="{{ $year }}">{{ $year }}</option>
+                                @endfor
+                            </select>
+
+                            <!-- Month -->
+                            <select name="birth_month" class="text-sm bg-[#dde0e5] w-1/3 h-8 rounded-sm focus:outline-none" required>
+                                <option value="">Month</option>
+                                @for($month = 1; $month <= 12; $month++)
+                                    <option value="{{ str_pad($month, 2, '0', STR_PAD_LEFT) }}">{{ date('F', mktime(0,0,0,$month,1)) }}</option>
+                                @endfor
+                            </select>
+
+                            <!-- Day -->
+                            <select name="birth_day" class="text-sm bg-[#dde0e5] w-1/3 h-8 rounded-sm focus:outline-none" required>
+                                <option value="">Day</option>
+                                @for($day = 1; $day <= 31; $day++)
+                                    <option value="{{ str_pad($day, 2, '0', STR_PAD_LEFT) }}">{{ $day }}</option>
+                                @endfor
+                            </select>
+                        </div>
                     </div>
 
-                    <!-- Empty field 1 (col-span-4) -->
+                    <!-- Year Level -->
                     <div class="flex flex-col col-span-4">
-                        <label for="">&nbsp;</label>
-                        <input type="text" name="" placeholder="mm" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
-                    </div>
-
-                    <!-- Empty field 2 (col-span-4) -->
-                    <div class="flex flex-col col-span-4">
-                        <label for="">&nbsp;</label>
-                        <input type="text" name="" placeholder="dd" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
-                    </div>
-
-                    <!-- Year Level (col-span-4) -->
-                    <div class="flex flex-col col-span-4">
-                        <label for="">Year Level</label>
-                        <select name="" id="" class="text-sm bg-[#dde0e5] w-full h-8 rounded-sm focus:outline-none">
-                            <option value="">1st year</option>
-                            <option value="">2nd year</option>
-                            <option value="">3rd year</option>
-                            <option value="">4th year</option>
-                            <option value="">5th year</option>
+                        <label for="year_level">Year Level</label>
+                        <select name="year_level" id="year_level" class="text-sm bg-[#dde0e5] w-full h-8 rounded-sm focus:outline-none" required>
+                            <option value="">Select Year</option>
+                            <option value="1st Year">1st Year</option>
+                            <option value="2nd Year">2nd Year</option>
+                            <option value="3rd Year">3rd Year</option>
+                            <option value="4th Year">4th Year</option>
+                            <option value="5th Year">5th Year</option>
                         </select>
                     </div>
 
-                    <!-- Course (col-span-8) -->
+                    <!-- Course -->
                     <div class="flex flex-col col-span-8">
-                        <label for="">Course</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="course">Course</label>
+                        <input type="text" name="course" id="course" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
                     </div>
 
-                    <!-- Year Level (col-span-12) -->
+                    <!-- Password -->
                     <div class="flex flex-col col-span-12">
-                        <label for="">Create Password</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
                     </div>
 
-                    <!-- Course (col-span-12) -->
+                    <!-- Confirm Password -->
                     <div class="flex flex-col col-span-12">
-                        <label for="">Confirm Password</label>
-                        <input type="text" name="" id="" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="bg-[#dde0e5] w-full h-8 p-2 rounded-sm focus:outline-none" required>
+                        <p id="password-error" class="text-red-500 text-sm mt-1 hidden">Passwords do not match</p>
                     </div>
+
                 </div>
 
                 <div class="flex justify-center mt-5">
@@ -91,4 +106,6 @@
         </form>
     </div>
 </div>
+
+
 @endsection
